b92c2e28cefba2bfe5552ec71b23dce001f7bbec
kingshing <528500308@qq.com>
2018-01-26 21:23:33 +0800

    format

--- src/com/kingshing/controllers/HelloController.java
+++ src/com/kingshing/controllers/HelloController.java
@@ -1,6 +1,5 @@
 package com.kingshing.controllers;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,18 +15,21 @@ import com.kingshing.entries.User;
 
 @Controller
 public class HelloController {
-	@RequestMapping(value="/hello.do")
-	public String say(String name,Model model) {
-		model.addAttribute("name", "=="+name);
+	@RequestMapping(value = "/hello.do")
+	public String say(String name, Model model) {
+		model.addAttribute("name", "==" + name);
 		return "home";
 	}
+
 	/**
-	 *  ajax返回string
-	 * @param name   <input name="name" />
+	 * ajax返回string
+	 * 
+	 * @param name
+	 *            <input name="name" />
 	 * @param response
 	 */
-	@RequestMapping(value="/ajax.do")
-	public void testAjax(String name,HttpServletResponse response) {
+	@RequestMapping(value = "/ajax.do")
+	public void testAjax(String name, HttpServletResponse response) {
 		response.setContentType("text/html");
 		System.out.println(name);
 		try {
@@ -38,16 +40,15 @@ public class HelloController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-	@RequestMapping(value="/ajaxObj.do")
+
+	@RequestMapping(value = "/ajaxObj.do")
 	public @ResponseBody User testAjaxObj(@ModelAttribute User user) {
 		System.out.println(user);
-		User u  =new User();
+		User u = new User();
 		u.setName("kingshing");
 		u.setPassword("123");
-		return u;		
+		return u;
 	}
 }
